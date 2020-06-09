a35c0bee4d915df424dd70bca10c7a19c91a8250
Ramrao <ramraodesai009@gmail.com>
2020-06-09 22:53:35 +0530

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListEmployeeComponent } from './employee/list-employee.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+
+
+const appRoutes: Routes = [
+  { path: 'list', component:ListEmployeeComponent },
+  { path: 'create', component:CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
